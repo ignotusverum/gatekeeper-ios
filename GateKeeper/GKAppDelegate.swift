5796fb6f8032188dd8a9f4f3213369880edce77a
Vlad Z <vladislavz1991@gmail.com>
2016-09-19 19:13:44 -0400

    initial commit

---
+++ GateKeeper/GKAppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  GateKeeper
+//
+//  Created by Vladislav Zagorodnyuk on 9/19/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
