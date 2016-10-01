fde4db952d4c88530dacd60f79f93852d59dc4bc
Vlad Z <vladislavz1991@gmail.com>
2016-10-01 18:11:56 -0400

    table view initial logic

--- GateKeeper/Secions/Account/GKAccountViewController.swift
+++ GateKeeper/Secions/Account/GKAccountViewController.swift
@@ -20,6 +20,48 @@ class GKAccountViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Init custom cells
+        self.tableViewInit()
+    }
+    
+    // Table view init
+    func tableViewInit() {
+     
+        // Account cells
+        let accountCellNib = UINib(nibName: "GKAccountTableViewCell", bundle: nil)
+        let accountActionCellNib = UINib(nibName: "GKActionAccountTableViewCell", bundle: nil)
         
+        // Register custom cells
+        self.tableView.register(accountCellNib, forCellReuseIdentifier: "GKAccountTableViewCell")
+        self.tableView.register(accountActionCellNib, forCellReuseIdentifier: "accountActionCellNib")
+    }
+}
+
+// MARK: - TableView Datasource
+extension GKAccountViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        return cell
+    }
+}
+
+// MARK: - TableView Delegate
+extension GKAccountViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    
+        // Text field in cell become responder
+        let cell = tableView.cellForRow(at: indexPath) as? GKAccountTableViewCell
+        cell?.textField.becomeFirstResponder()
     }
 }
