bd601ed5a48cf7ac0649ed4066d614630c22e60a
Vlad Z <vladislavz1991@gmail.com>
2016-10-01 17:29:56 -0400

    account controller initial logic

---
+++ GateKeeper/Secions/Account/GKAccountViewController.swift
@@ -0,0 +1,15 @@
+//
+//  GKAccountViewController.swift
+//  GateKeeper
+//
+//  Created by Vladislav Zagorodnyuk on 10/1/16.
+//  Copyright © 2016 Vlad Z. All rights reserved.
+//
+
+import UIKit
+
+class GKAccountViewController: UIViewController {
+
+    
+
+}
